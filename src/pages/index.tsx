8454b20fa3d3ea78384af55638901778cebb8e46
Yongveloper <aaa9015@naver.com>
2023-11-02 23:21:19 +0900

    style: 사용되지 않는 섹션 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,40 +4,25 @@ import path from "path";
 import { NextPage } from "next";
 
 import Activity from "@/components/Activity";
-import Certificate from "@/components/Certificate";
 import Education from "@/components/Education";
 import Footer from "@/components/Footer";
 import Information from "@/components/Information";
 import Layout from "@/components/Layout";
 import Project from "@/components/Project";
 import ResumeTitle from "@/components/ResumeTitle";
-// import ScrollProgress from "@/components/ScrollProgress";
-import WorkExperience from "@/components/WorkExperience";
+import ScrollProgress from "@/components/ScrollProgress";
 import { DataProps, InformationProps, ProjectProps, WorkExperienceProps } from "@/types";
-import Award from "@/components/Award";
 
-const Home: NextPage<DataProps> = ({
-  resumeTitle,
-  information,
-  workExperience,
-  project,
-  activity,
-  education,
-  certificate,
-  award,
-}) => {
+const Home: NextPage<DataProps> = ({ resumeTitle, information, project, activity, education }) => {
   return (
     <>
-      {/* <ScrollProgress /> */}
+      <ScrollProgress />
       <ResumeTitle resumeTitle={resumeTitle} />
       <Layout>
         <Information information={information} />
-        <WorkExperience workExperience={workExperience} />
         <Project project={project} />
         <Activity activity={activity} />
         <Education education={education} />
-        <Certificate certificate={certificate} />
-        <Award award={award} />
       </Layout>
       <Footer contact={information.contact} name={information.name} />
     </>
@@ -56,15 +41,6 @@ export const getStaticProps = async () => {
     item: await getMd({ section: "information", item: { ...objectData.information } }),
   });
 
-  const workExperienceWithData = objectData.workExperience.map(
-    async (item: WorkExperienceProps) => {
-      return getImgSrc({
-        section: "workExperience",
-        item: await getMd({ section: "workExperience", item }),
-      });
-    },
-  );
-
   const projectWithData = objectData.project.map(async (item: ProjectProps) => {
     return getImgSrc({ section: "project", item: await getMd({ section: "project", item }) });
   });
@@ -73,7 +49,6 @@ export const getStaticProps = async () => {
     props: {
       ...objectData,
       information: await informationWithData,
-      workExperience: await Promise.all(workExperienceWithData),
       project: await Promise.all(projectWithData),
     },
   };
