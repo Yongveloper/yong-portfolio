cd2e9d72733364d0cfb806771a17a815fa5031ae
Yongveloper <aaa9015@naver.com>
2023-12-30 02:20:32 +0900

    style:  사용하지 않는 폰트 삭제

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <link
-          href="https://hangeul.pstatic.net/hangeul_static/css/nanum-gothic.css"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head></Head>
       <body className="font-normal break-keep selection:bg-PRIMARY_LIGHT selection:dark:text-BLACK text-BLACK dark:bg-BLACK dark:text-white">
         <Main />
         <NextScript />
